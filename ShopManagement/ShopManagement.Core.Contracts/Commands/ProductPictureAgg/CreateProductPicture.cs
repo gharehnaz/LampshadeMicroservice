ce42ea0ca0006a854f964676ab579e15f394690c
mahdi bagheri <gharehnaz71@outlook.com>
2024-06-14 18:37:45 +0330

    command contract

---
+++ ShopManagement/ShopManagement.Core.Contracts/Commands/ProductPictureAgg/CreateProductPicture.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopManagement.Core.Contracts.Commands.ProductPictureAgg
+{
+    public class CreateProductPicture
+    {
+        public long ProductId { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+    public class ProductViewModel
+    {
+        public long Id { get; set; }
+        public string Picture { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Category { get; set; }
+        public long CategoryId { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
