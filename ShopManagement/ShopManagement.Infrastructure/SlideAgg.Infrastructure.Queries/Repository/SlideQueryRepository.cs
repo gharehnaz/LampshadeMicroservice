75c717d1088d2dc6e1c5b825c82733eb304c54ce
mahdi bagheri <gharehnaz71@outlook.com>
2024-06-15 14:51:37 +0330

    slide infra

--- ShopManagement/ShopManagement.Infrastructure/SlideAgg.Infrastructure.Queries/Repository/SlideQueryRepository.cs
+++ ShopManagement/ShopManagement.Infrastructure/SlideAgg.Infrastructure.Queries/Repository/SlideQueryRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace SlideAgg.Infrastructure.Queries.Repository
+﻿using ShopManagement.Core.Contracts.IRepositories.ISlide;
+using ShopManagement.Core.Contracts.Queries.SlideAgg;
+
+namespace SlideAgg.Infrastructure.Queries.Repository
 {
-    public class SlideQueryRepository
+    public class SlideQueryRepository: ISlideQueryRepository
     {
+        private readonly SlideQueryDbContext _context;
+        public SlideQueryRepository(SlideQueryDbContext context)
+        {
+
+            _context = context;
+
+        }
+
+        public async Task<GetSlideDetailsResult> Query(GetSlideDetails getSlideDetails)
+        {
+            return _context.Slides.Select(x => new GetSlideDetailsResult
+            {
+                Id = x.Id,
+                BtnText = x.BtnText,
+                Heading = x.Heading,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Text = x.Text,
+                Link = x.Link,
+                Title = x.Title
+            }).FirstOrDefault(x => x.Id == getSlideDetails.Id);
+        }
+
+        public async Task<List<GetSlidesResult>> Query(GetSlides getSlides)
+        {
+            return _context.Slides.Select(x => new GetSlidesResult
+            {
+                Id = x.Id,
+                Heading = x.Heading,
+                Picture = x.Picture,
+                Title = x.Title,
+                IsRemoved = x.IsRemoved,
+            }).OrderByDescending(x => x.Id).ToList();
+        }
     }
 }
--- ShopManagement/ShopManagement.Infrastructure/SlideAgg.Infrastructure.Queries/SlideQueryDbContext.cs
+++ ShopManagement/ShopManagement.Infrastructure/SlideAgg.Infrastructure.Queries/SlideQueryDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace SlideAgg.Infrastructure.Queries
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SlideAgg.Infrastructure.Queries
 {
-    public class SlideQueryDbContext
+    public class SlideQueryDbContext:DbContext
     {
+        public SlideQueryDbContext(DbContextOptions<SlideQueryDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Slide> Slides { get; set; }
     }
 }
