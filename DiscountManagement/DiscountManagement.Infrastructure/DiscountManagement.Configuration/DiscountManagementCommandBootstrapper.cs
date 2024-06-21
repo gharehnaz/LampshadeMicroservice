d01007dc40bf601b117923861623e2fadea25ae8
mahdi bagheri <gharehnaz71@outlook.com>
2024-06-21 13:56:08 +0330

    ef install

--- DiscountManagement/DiscountManagement.Infrastructure/DiscountManagement.Configuration/DiscountManagementCommandBootstrapper.cs
+++ DiscountManagement/DiscountManagement.Infrastructure/DiscountManagement.Configuration/DiscountManagementCommandBootstrapper.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscountManagement.Configuration
+﻿namespace DiscountManagement.Configuration
 {
-    internal class DiscountManagementCommandBootstrapper
+    public class DiscountManagementCommandBootstrapper
     {
     }
 }
--- DiscountManagement/DiscountManagement.Infrastructure/DiscountManagement.Configuration/DiscountManagementQueryBootstrapper.cs
+++ DiscountManagement/DiscountManagement.Infrastructure/DiscountManagement.Configuration/DiscountManagementQueryBootstrapper.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscountManagement.Configuration
+﻿namespace DiscountManagement.Configuration
 {
-    internal class DiscountManagementQueryBootstrapper
+    public class DiscountManagementQueryBootstrapper
     {
     }
 }
