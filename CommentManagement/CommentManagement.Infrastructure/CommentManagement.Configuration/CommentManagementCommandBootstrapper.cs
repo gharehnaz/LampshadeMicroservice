57709a6afce710b73a732bd733dbc4d82d4953ea
mahdi bagheri <gharehnaz71@outlook.com>
2024-06-21 18:34:36 +0330

    comment infra

--- CommentManagement/CommentManagement.Infrastructure/CommentManagement.Configuration/CommentManagementCommandBootstrapper.cs
+++ CommentManagement/CommentManagement.Infrastructure/CommentManagement.Configuration/CommentManagementCommandBootstrapper.cs
@@ -1,6 +1,18 @@
-﻿namespace CommentManagement.Configuration
+﻿using CommentManagement.Core.Contracts.IRepositories.IComment;
+using CommentManagement.Infrastructure.Command;
+using CommentManagement.Infrastructure.Command.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommentManagement.Configuration
 {
     public class CommentManagementCommandBootstrapper
     {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ICommentCommandRepository, CommentManagementCommandRepository>();
+
+            services.AddDbContext<CommentManagementCommandDbContext>(x => x.UseSqlServer(connectionString));
+        }
     }
 }
--- CommentManagement/CommentManagement.Infrastructure/CommentManagement.Configuration/CommentManagementQueryBootstrapper.cs
+++ CommentManagement/CommentManagement.Infrastructure/CommentManagement.Configuration/CommentManagementQueryBootstrapper.cs
@@ -1,6 +1,18 @@
-﻿namespace CommentManagement.Configuration
+﻿using CommentManagement.Core.Contracts.IRepositories.IComment;
+using CommentManagement.Infrastructure.Command;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CommentManagement.Infrastructure.Query.Repository;
+
+namespace CommentManagement.Configuration
 {
     public class CommentManagementQueryBootstrapper
     {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ICommentQueryRepository, CommentManagementQueryRepository>();
+
+            services.AddDbContext<CommentManagementCommandDbContext>(x => x.UseSqlServer(connectionString));
+        }
     }
 }
